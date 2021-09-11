a273b3519823b282ef4563c4d299690414c15e61
tgablin <53415701+tgablin@users.noreply.github.com>
2021-09-11 14:43:00 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#This is called an Octothorpe, it allows you to leave comments in code.
+#Anything after the Octothorpe is ignored by Python
+
+print("I could have code like this.")
+
+#You can use comments to "disable" or prevent code from running
+#print("This won't run")
+
+print("This will run")
